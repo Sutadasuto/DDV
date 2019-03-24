3885c831e0d1c2eaa60f0e290f3cfae07aba197a
Sutadasuto <rodrigo.rill@hotmail.com>
2019-03-23 22:33:35 -0600

    minor update

--- keras_tools/utilities.py
+++ keras_tools/utilities.py
@@ -4,6 +4,7 @@ import os
 def get_modality_views(modality_folder):
 
     views = sorted([os.path.join(modality_folder, f) for f in os.listdir(modality_folder)
-                      if os.path.isdir(os.path.join(modality_folder, f)) and not f.startswith('.')],
+                      if os.path.isdir(os.path.join(modality_folder, f)) and not f.startswith('.')
+                    and f.lower() != "all"],
                      key=lambda f: f.lower())
     return tuple(views)
